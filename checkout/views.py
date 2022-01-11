ccf486b12728d1efbd642b352165febfce55c49a
VictoriaShalabaeva <shalabaevavt@gmail.com>
2022-01-11 16:41:48 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KGmUPDwr3ltShcFrFoLeTSSLPXcx384f4SRIO356sJbHqDuOczvNrRyhIGIVTNIE4jC88HIjZG8xiFYUzy8r37z00GJACTDFF',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

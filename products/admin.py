ee17c255b095d76d86048e5b834f0822dcedd45e
VictoriaShalabaeva <shalabaevavt@gmail.com>
2022-01-17 12:34:40 +0000

    Updated admin products

--- products/admin.py
+++ products/admin.py
@@ -31,23 +31,24 @@ from .models import Product, Category
 
 # Register your models here.
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'sku',
-#         'name',
-#         'category',
-#         'price',
-#         'rating',
-#         'image',
-#     )
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'number_id',
+        'name',
+        'category',
+        'brand',
+        'price',
+        'rating',
+        'image_url',
+    )
 
-#     ordering = ('sku',)
+    ordering = ('number_id',)
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'friendly_name',
-#         'name',
-#     )
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
 
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -51,7 +51,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    number_id = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    number_id = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=254)
     brand = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)

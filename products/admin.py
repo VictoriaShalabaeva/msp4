d22fdb22d74d9c394575634346cf1c680ac9d7b1
VictoriaShalabaeva <shalabaevavt@gmail.com>
2022-01-17 00:19:46 +0000

    Formatted new database as fixtures

--- products/admin.py
+++ products/admin.py
@@ -1,25 +1,25 @@
-from django.contrib import admin
-from .models import Product, Category
+# from django.contrib import admin
+# from .models import Product, Category
 
-# Register your models here.
+# # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'sku',
+#         'name',
+#         'category',
+#         'price',
+#         'rating',
+#         'image',
+#     )
 
-    ordering = ('sku',)
+#     ordering = ('sku',)
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'friendly_name',
+#         'name',
+#     )
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Category, CategoryAdmin)

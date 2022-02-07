c5dbbbe11b6e75eac9dfe7b05d75c12ca78a8f08
VictoriaShalabaeva <shalabaevavt@gmail.com>
2022-02-07 12:33:03 +0000

    Added wishlist modal

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Wishlist
 
 # Register your models here.
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_profile',
+        # 'number_id',
+        # 'name',
+        'product_color',
+        # 'price'
+    )
+
+admin.site.register(Wishlist, WishlistAdmin)
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
 
 # Create your models here.
+
+class Wishlist(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='wishlist')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_color = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.user_profile

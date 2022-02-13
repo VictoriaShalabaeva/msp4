083128863bd781cb1869e95ef0ac0d7c04d3c91c
VictoriaShalabaeva <shalabaevavt@gmail.com>
2022-02-13 11:44:24 +0000

    Modified views to render wishlist

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -2,86 +2,43 @@
 # from django.shortcuts import get_object_or_404
 # from products.models import Product
 
+
 # def wishlist_contents(request):
 
 #     wishlist_items = []
-#     wishlist = request.session.get('wishlist', {})
+#     # total = 0
+#     product_count = 0
+#     wishlist = None
+#     try:
+#         wishlist = Wishlist.objects.get(user=request.user)
+#     except Wishlist.DoesNotExist:
+#         pass
 
 #     for item_id, item_data in wishlist.items():
-#         product = get_object_or_404(Product, pk=item_id)
-#         for color in item_data['items_by_color']:
+#         if isinstance(item_data, int):
+#             product = get_object_or_404(Product, pk=item_id)
+#             # total += item_data * product.price
+#             product_count += item_data
 #             wishlist_items.append({
 #                 'item_id': item_id,
+#                 'quantity': item_data,
 #                 'product': product,
-#                 'color': color,
 #             })
+#         else:
+#             product = get_object_or_404(Product, pk=item_id)
+#             for color, quantity in item_data['items_by_color'].items():
+#                 # total += quantity * product.price
+#                 product_count += quantity
+#                 wishlist_items.append({
+#                     'item_id': item_id,
+#                     'quantity': quantity,
+#                     'product': product,
+#                     'color': color,
+#                 })
     
 #     context = {
 #         'wishlist_items': wishlist_items,
+#         'product_count': product_count,
 #     }
 
 #     return context
-
-
-# from django.conf import settings
-# from django.shortcuts import get_object_or_404
-# from products.models import Product
-
-# def wishlist_contents(request):
-
-#     wishlist_items = []
-#     wishlist = request.session.get('wishlist', {})
-
-#     for item_id in wishlist.items():
-#         product = get_object_or_404(Product, pk=item_id)
-#         wishlist_items.append({
-#             'item_id': item_id,
-#             'product': product,
-#         })
-    
-#     context = {
-#         'wishlist_items': wishlist_items,
-#     }
-
-#     return context
-
-
-from django.conf import settings
-from django.shortcuts import get_object_or_404
-from products.models import Product
-
-def wishlist_contents(request):
-
-    wishlist_items = []
-    # total = 0
-    product_count = 0
-    wishlist = request.session.get('wishlist', {})
-
-    for item_id, item_data in wishlist.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            # total += item_data * product.price
-            product_count += item_data
-            wishlist_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for color, quantity in item_data['items_by_color'].items():
-                # total += quantity * product.price
-                product_count += quantity
-                wishlist_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'color': color,
-                })
-    
-    context = {
-        'wishlist_items': wishlist_items,
-        'product_count': product_count,
-    }
-
-    return context
--- wishlist/views.py
+++ wishlist/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, HttpResponse, get_object
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Wishlist
+from .models import Wishlist, WishlistLineItem
 from products.models import Product
 from profiles.models import UserProfile
 
@@ -11,8 +11,12 @@ from profiles.models import UserProfile
 @login_required
 def view_wishlist(request):
     """ A view that renders the wishlist contents page """
+    user_wishlist = get_object_or_404(Wishlist, user=request.user)
+    wishlist_items = WishlistLineItem.objects.filter(wishlist=user_wishlist)
+    # user_wishlist = Wishlist.objects.get_or_create(request.user)
 
-    return render(request, 'wishlist/wishlist.html')
+    return render(request, 'wishlist/wishlist.html', {"wishlist_items": wishlist_items})
+
 
 @login_required
 def add_to_wishlist(request, item_id):
@@ -24,28 +28,53 @@ def add_to_wishlist(request, item_id):
     color = None
     if 'product_color' in request.POST:
         color = request.POST['product_color']
-    wishlist = request.session.get('wishlist', {})
+    user_wishlist, _ = Wishlist.objects.get_or_create(request.user)
 
-    if color:
-        if item_id in list(wishlist.keys()):
-            if color in wishlist[item_id]['items_by_color'].keys():
-                wishlist[item_id]['items_by_color'][color] += quantity
-            else:
-                wishlist[item_id]['items_by_color'][color] = quantity
-                messages.success(request, f'Added color {color} {product.name} to your wishlist')
-        else:
-            wishlist[item_id] = {'items_by_color': {color: quantity}}
-            messages.success(request, f'Added color {color} {product.name} to your wishlist')
-    else:
-        if item_id in list(wishlist.keys()):
-            wishlist[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {wishlist[item_id]}')
-        else:
-            wishlist[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your wishlist')
+    # wishlist_items = {
+    #     wishlist_item.product.id: wishlist_item
+    #     for wishlist_item in WishlistLineItem.objects.filter(wishlist=user_wishlist)
+    # }
 
-    request.session['wishlist'] = wishlist
-    return redirect(redirect_url)
+
+    if request.method == 'POST':
+        product_wishlisted = False
+        if color:
+            product_wishlisted = WishlistLineItem.objects.filter(wishlist=user_wishlist, product=product, product_color=color).exists()
+        else:
+            product_wishlisted = WishlistLineItem.objects.filter(wishlist=user_wishlist, product=product).exists()
+
+        if product_wishlisted:
+            # wishlist_item = wishlist_items[product.id]
+            # if wishlist_item.product_color == color:
+                messages.warning(request, 'Product already in wishlit.')
+        else:
+            wishlist_item = WishlistLineItem.objects.create(
+                wishlist=user_wishlist, product=product,product_color=color)
+            # user_wishlist.products.add(wishlist_item)
+            messages.success(request, f'Added {product.name} ({color}) to wishlist.')
+
+    
+
+
+        # if color:
+        #     if item_id in list(user_wishlist.keys()):
+        #         if color in user_wishlist[item_id]['items_by_color'].keys():
+        #             user_wishlist[item_id]['items_by_color'][color] += quantity
+        #         else:
+        #             user_wishlist[item_id]['items_by_color'][color] = quantity
+        #             messages.success(request, f'Added color {color} {product.name} to your user_wishlist')
+        #     else:
+        #         user_wishlist[item_id] = {'items_by_color': {color: quantity}}
+        #         messages.success(request, f'Added color {color} {product.name} to your user_wishlist')
+        # else:
+        #     if item_id in list(user_wishlist.keys()):
+        #         user_wishlist[item_id] += quantity
+        #         messages.success(request, f'Updated {product.name} quantity to {user_wishlist[item_id]}')
+        #     else:
+        #         user_wishlist[item_id] = quantity
+        #         messages.success(request, f'Added {product.name} to your wishlist')
+
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
@@ -57,7 +86,7 @@ def remove_from_wishlist(request, item_id):
         color = None
         if 'product_color' in request.POST:
             color = request.POST['product_color']
-        wishlist = request.session.get('wishlist', {})
+        wishlist = Wishlist.objects.get_or_create('wishlist', {})
 
         if color:
             del wishlist[item_id]['items_by_color'][color]
@@ -68,7 +97,8 @@ def remove_from_wishlist(request, item_id):
             wishlist.pop(item_id)
             messages.success(request, f'Removed {product.name} from your wishlist')
 
-        request.session['wishlist'] = wishlist
+        #request.session['wishlist'] = wishlist
+        return redirect(request.META.get('HTTP_REFERER'))
         return HttpResponse(status=200)
 
     except Exception as e:

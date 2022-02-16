9209dff854a617b7304760287405fe512db10ae4
VictoriaShalabaeva <shalabaevavt@gmail.com>
2022-02-16 14:21:07 +0000

    Checked home folder

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,7 @@
+"""Code credit: the code is written following the Code Institute tutorials
+(Boutique Ado Project).
+"""
+
 from django.urls import path
 from . import views
 
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
+"""Code credit: the code is written following the Code Institute tutorials
+(Boutique Ado Project).
+"""
+
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
